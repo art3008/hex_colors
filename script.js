e2cf56254d6189f7ed6c09460a9a13050608bdbc
art3008 <art.osv@mail.ru>
2022-10-29 13:34:11 +0600

    done

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-btn = document.querySelectorAll(".change_color")
+const btn = document.querySelector(".change_color")
+const descript = document.querySelector('.description')
+
 
 btn.addEventListener('click', () =>{
     console.log('hello');
+
+    descript.style.color = 'red'
 })
+
+
+
