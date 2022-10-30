5072dc947c12f51a5d2e14bb6e7e81c17b71cea4
art3008 <art.osv@mail.ru>
2022-10-30 11:40:34 +0600

    done

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+//Попробовать сделать через обработчик событий
+
+
 const btn = document.querySelector(".change_color")
 const descript = document.querySelector('.description')
-const hexText = document.querySelector('.hex_text')
+const headBlock = document.querySelector('.head_block')
+
 
 let arrayLettr = ['0','1','2','3','4','5','6','7',
 '8','9','a','b','c','d','e','f']
 
 let hex_code = ''
 
-
-
 btn.addEventListener('click', () =>{
     for(let i = 0; i < 6; i++) {
+        
         let random = Math.floor(Math.random() * arrayLettr.length)
-    
+        
         hex_code += arrayLettr[random]
     
     }
     
-    // hexText.style.color = '#32a88d'
-    console.log(hex_code)
-    hex_code=''
+    headBlock.style.background = `linear-gradient(to right, #${hex_code}, yellow)`   ;
+    console.log(hex_code);
+    hex_code = ''
+
 })
 
+
+
+    
 
 
